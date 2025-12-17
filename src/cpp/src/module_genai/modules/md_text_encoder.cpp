933b0605652d84cf877a070085d086dbe33a0028
plusbang <binbin1.deng@intel.com>
2025-12-17 14:10:45 +0800

    add text encoder

--- src/cpp/src/module_genai/modules/md_text_encoder.cpp
+++ src/cpp/src/module_genai/modules/md_text_encoder.cpp
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "md_text_encoder.hpp"
+#include "openvino/genai/tokenizer.hpp"
+#include "tokenizer/tokenizer_impl.hpp"
 
 #include <chrono>
 #include <thread>
@@ -14,11 +16,48 @@ TextEncoderModule::TextEncoderModule(const IBaseModuleDesc::PTR& desc) : IBaseMo
     
 }
 
+bool TextEncoderModule::initialize() {
+    const auto& params = module_desc->params;
+    auto it_path = params.find("model_path");
+    if (it_path == params.end()) {
+        std::cerr << "TextEncoderModule[" << module_desc->name << "]: 'model_path' not found in params" << std::endl;
+        return false;
+    }
+    
+    std::filesystem::path tokenizer_path = it_path->second;
+    return initialize(tokenizer_path, m_tokenization_params);
+}
+
 void TextEncoderModule::run() {
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
     std::cout << "Run: " << ModuleTypeConverter::toString(static_cast<ModuleType>(module_desc->type)) << "["
               << module_desc->name << "]" << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    
+    // TODO: 从 inputs 中获取 prompt 数据
+    if (m_prompt.empty()) {
+        return;
+    }
+    auto encoded = run(m_prompt);
+    // TODO: 设置outputs
+}
+
+bool TextEncoderModule::initialize(const std::filesystem::path& tokenizer_path,
+                                   const ov::AnyMap& properties) {
+    m_tokenizer_impl = std::make_shared<Tokenizer::TokenizerImpl>(tokenizer_path, properties);
+    m_tokenization_params = properties;
+    return true;
+}
+
+TokenizedInputs TextEncoderModule::run(const std::string& prompt) {
+    // TODO: 似乎暂时缺少各模块的initiate？hard code here
+    std::filesystem::path tokenizer_path = "/mnt/disk2/Qwen2.5-VL-7B-Instruct-INT4";
+    m_tokenizer_impl = std::make_shared<Tokenizer::TokenizerImpl>(tokenizer_path, m_tokenization_params);
+
+    OPENVINO_ASSERT(m_tokenizer_impl, "TextEncoderModule is not initialized. Call initialize() first.");
+    check_arguments(m_tokenization_params, {ov::genai::add_special_tokens.name(),
+                                            ov::genai::max_length.name(),
+                                            ov::genai::pad_to_max_length.name(),
+                                            ov::genai::padding_side.name()});
+    return m_tokenizer_impl->encode(prompt, m_tokenization_params);
 }
 
 }  // namespace module
--- src/cpp/src/module_genai/modules/md_text_encoder.hpp
+++ src/cpp/src/module_genai/modules/md_text_encoder.hpp
@@ -7,6 +7,8 @@
 
 #include "module_genai/module.hpp"
 #include "module_genai/module_type.hpp"
+#include "openvino/genai/tokenizer.hpp"
+#include "tokenizer/tokenizer_impl.hpp"
 
 namespace ov {
 namespace genai {
@@ -24,10 +26,21 @@ public:
 
     void run() override;
 
+    bool initialize(const std::filesystem::path& tokenizer_path,
+                    const ov::AnyMap& properties = {});
+
+    // TODO: 假定只有单条 prompt
+    TokenizedInputs run(const std::string& prompt);
+
     using PTR = std::shared_ptr<TextEncoderModule>;
     static PTR create(const IBaseModuleDesc::PTR& desc) {
         return PTR(new TextEncoderModule(desc));
     }
+
+private:
+    std::shared_ptr<Tokenizer::TokenizerImpl> m_tokenizer_impl;
+    ov::AnyMap m_tokenization_params = {};
+    std::string m_prompt = "Describe this image.";
 };
 
 }  // namespace module
