dca8255ce5089992bd0216a98ea8be87ce1e7cdf
Yan <xiping.yan@intel.com>
2025-12-21 16:12:05 +0800

    tmp

--- samples/cpp/module_genai/ut_modules.cpp
+++ samples/cpp/module_genai/ut_modules.cpp
@@ -19,7 +19,16 @@ static bool print_subword(std::string &&subword)
     return !(std::cout << subword << std::flush);
 }
 
-int test_genai_module_pipeline(int argc, char *argv[])
+static std::vector<std::string> register_all_test_modules()
+{
+    // Register custom modules here based on config.yaml used in tests.
+    std::vector<std::string> module_config_yamls = {
+        "config_module/config_textencodermodule.yaml",
+        "config_module/config_imagepreprocessingmodule.yaml"
+    };
+}
+
+int test_genai_module_ut_modules(int argc, char *argv[])
 {
     std::cout << "== Init ModulePipeline" << std::endl;
     std::string config_fn = get_config_ymal_path(argc, argv);
@@ -53,19 +62,6 @@ int test_genai_module_pipeline(int argc, char *argv[])
 
         auto output_raw_data = pipe.get_output("raw_data").as<ov::Tensor>();
         std::cout << "Output raw data first value: " << output_raw_data.data<float>()[0] << std::endl;
-    }
-    return EXIT_SUCCESS;
-}
-
-int main(int argc, char *argv[])
-{
-    try
-    {
-        test_genai_module_pipeline(argc, argv);
-    }
-    catch (const std::exception &error)
-    {
-        std::cerr << "Catch exceptions: " << error.what() << '\n';
     }
     return EXIT_SUCCESS;
 }
--- samples/cpp/module_genai/ut_pipelines.sh
+++ samples/cpp/module_genai/ut_pipelines.sh
@@ -7,4 +7,4 @@ source ../../../../openvino_toolkit_ubuntu24_2025.4.0.20398.8fdad55727d_x86_64/s
 
 cd ${SCRIPT_DIR_EXAMPLE_OV_CPP_RUN}
 
-./build/module_genai_app ./config_pipeline/config_qwen2_5_vl.yaml
+./build/module_genai_ut_app ut_pipelines
