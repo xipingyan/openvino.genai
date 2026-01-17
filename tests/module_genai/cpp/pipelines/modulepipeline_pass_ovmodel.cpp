9b7d74e9672bb71370cca1cc5cc55b8430bd6b0d
xipingyan <xiping.yan@intel.com>
2026-01-17 20:34:56 +0800

    test

--- tests/module_genai/cpp/pipelines/modulepipeline_pass_ovmodel.cpp
+++ tests/module_genai/cpp/pipelines/modulepipeline_pass_ovmodel.cpp
@@ -5,6 +5,7 @@
 
 #include <chrono>
 #include <filesystem>
+#include <openvino/openvino.hpp>
 #include <openvino/genai/module_genai/pipeline.hpp>
 #include <thread>
 
@@ -28,32 +29,49 @@
 // Define test parameters:
 // bool: Single ov::Model or multiple ov::Model;
 // bool: main module or submodule use model from models_map
-
 using test_params = std::tuple<bool, bool>;
 
 // Define Dummy Modules for testing
 namespace ov::genai::module {
 
-static std::thread::id dummy_module_a_thread_id;
-static std::thread::id dummy_module_b_thread_id;
+static bool g_single_ov_model;
+static bool g_main_module_use_model;
+static int g_dummy_module_a_got_ovmodel_count = 0;
+static int g_dummy_module_b_got_ovmodel_count = 0;
 
 class DummyModuleA : public IBaseModule {
-    DeclareModuleConstructorDummy(DummyModuleA);
-
+protected:
+    DummyModuleA() = delete;
+    DummyModuleA(const IBaseModuleDesc::PTR& desc, const PipelineDesc::PTR& pipeline_desc)
+        : IBaseModule(desc, pipeline_desc) {
+        // First ov::Model will be init in IBaseModule constructor.
+        if (!g_single_ov_model) {
+            // Initialize second ov::Model from models_map
+            m_ov_model_2 = get_ov_model_from_cfg_models_map("ov_model_2", true);
+        }
+    }
 public:
+    ~DummyModuleA() {};
+    using PTR = std::shared_ptr<DummyModuleA>;
+    static PTR create(const IBaseModuleDesc::PTR& desc, const PipelineDesc::PTR& pipeline_desc) {
+        // Can't use std::make_shared with protected ctor.
+        return PTR(new DummyModuleA(desc, pipeline_desc));
+    }
+    static void print_static_config() {}
+
     void run() override {
-        if (get_name() == "dummy_module_a") {
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            dummy_module_a_thread_id = std::this_thread::get_id();
+        if (is_dummy_module_a()) {
+            g_dummy_module_a_got_ovmodel_count += (m_ov_model == nullptr ? 0 : 1);
+            g_dummy_module_a_got_ovmodel_count += (m_ov_model_2 == nullptr ? 0 : 1);
         } else {
-            std::this_thread::sleep_for(std::chrono::milliseconds(2));
-            dummy_module_b_thread_id = std::this_thread::get_id();
+            g_dummy_module_b_got_ovmodel_count += (m_ov_model == nullptr ? 0 : 1);
+            g_dummy_module_b_got_ovmodel_count = m_ov_model_2 == nullptr ? 1 : 2;
         }
     }
 
 private:
     std::shared_ptr<ov::Model> m_ov_model_2 = nullptr;
-    bool m_work_as_main_module = true;
+    bool is_dummy_module_a() { return (get_name() == "dummy_module_a"); }
 };
 
 REGISTER_MODULE_CONFIG(DummyModuleA);
@@ -71,12 +89,7 @@ public:
         const auto& single_ov_model = std::get<0>(obj.param);
         const auto& main_module_use_model = std::get<1>(obj.param);
         std::string result;
-        if (single_ov_model) {
-            result += "Pass1OVModel_";
-        }
-        else {
-            result += "PassMultiOVModel_";
-        }
+        result += single_ov_model ? "Pass1OVModel_" : "PassMultipleOVModel_";
         result += main_module_use_model ? "MainModuleUseOVModel" : "SubModuleUseOVModel";
         return result;
     }
@@ -84,6 +97,22 @@ public:
     void SetUp() override {
         REGISTER_TEST_NAME();
         std::tie(_single_ov_model, _main_module_use_model) = GetParam();
+        ov::genai::module::g_single_ov_model = _single_ov_model;
+        ov::genai::module::g_main_module_use_model = _main_module_use_model;
+
+        // load ov model.
+        ov::Core core;
+        auto model_1 = core.read_model(TEST_MODEL::Qwen2_5_VL_3B_Instruct_INT4() + "openvino_text_embeddings_model.xml");
+
+        m_models_map.clear();
+        std::string module_key = _main_module_use_model ? "dummy_module_a" : "dummy_module_b";
+        m_models_map[module_key].clear();
+        m_models_map[module_key]["ov_model"] = model_1;
+        if (!_single_ov_model) {
+            auto model_2 =
+                core.read_model(TEST_MODEL::Qwen2_5_VL_3B_Instruct_INT4() + "openvino_vision_embeddings_model.xml");
+            m_models_map[module_key]["ov_model_2"] = model_2;
+        }
     }
 
     void TearDown() override {}
@@ -159,6 +188,15 @@ protected:
     }
 
     void check_outputs(ov::genai::module::ModulePipeline& pipe) override {
+        // Check DummyModuleA got ov::Model count
+        int expected_model_count = _single_ov_model ? 1 : 2;
+        if (_main_module_use_model) {
+            EXPECT_EQ(ov::genai::module::g_dummy_module_a_got_ovmodel_count, expected_model_count);
+            EXPECT_EQ(ov::genai::module::g_dummy_module_b_got_ovmodel_count, 0);
+        } else {
+            EXPECT_EQ(ov::genai::module::g_dummy_module_a_got_ovmodel_count, 0);
+            EXPECT_EQ(ov::genai::module::g_dummy_module_b_got_ovmodel_count, expected_model_count);
+        }
     }
 };
 
