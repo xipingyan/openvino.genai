0a144c11f348030a4e7e1348c3031d01d5df7c37
xiping.yan <xiping.yan@intel.com>
2026-03-07 11:33:06 +0800

    pass string for cache_dir
    Signed-off-by: xiping.yan <xiping.yan@intel.com>

--- samples/cpp/module_genai/md_visual_language_chat.cpp
+++ samples/cpp/module_genai/md_visual_language_chat.cpp
@@ -59,16 +59,14 @@ int main(int argc, char* argv[]) {
             throw std::runtime_error(std::string{"Usage: "} + argv[0] +
                                      "\n"
                                      "  -cfg config.yaml \n"
-                                     "  -enable_cache_dir: [Optional] true/false, default false\n"
+                                     "  -cache_dir: [Optional] string path, default empty\n"
                                      "  -prompt: input prompt\n"
                                      "  -img: [Optional] image path\n"
                                      "  -video: [Optional] video path\n");
         }
 
         std::filesystem::path config_path = utils::get_input_arg(argc, argv, "-cfg", std::string{});
-        std::string enable_cache_dir_str = utils::get_input_arg(argc, argv, "-enable_cache_dir", std::string{"false"});
-        bool enable_cache_dir = (enable_cache_dir_str == "1" || enable_cache_dir_str == "true" ||
-                                 enable_cache_dir_str == "True" || enable_cache_dir_str == "TRUE");
+        std::string cache_dir = utils::get_input_arg(argc, argv, "-cache_dir", std::string{});
         std::string prompt = utils::get_input_arg(argc, argv, "-prompt", std::string{});
         std::string img_path = utils::get_input_arg(argc, argv, "-img", std::string{});
         std::string video_path = utils::get_input_arg(argc, argv, "-video", std::string{});
@@ -80,8 +78,8 @@ int main(int argc, char* argv[]) {
         }
 
         ov::AnyMap properties{};
-        if (enable_cache_dir) {
-            properties.insert({ov::cache_dir("vlm_cache")});
+        if (!cache_dir.empty()) {
+            properties.insert({ov::cache_dir(cache_dir)});
         }
 
         ov::genai::module::ModulePipeline pipe(config_path, properties);
