5dada4e8ef24c8bc8d4ce112e0d6f37f23e65c7f
xipingya <xiping.yan@intel.com>
2026-04-01 09:31:14 +0800

    qwen3 impl

---
+++ src/cpp/src/module_genai/modules/m_llm_inference_pa/models/qwen3_omni.cpp
@@ -0,0 +1,42 @@
+// Copyright (C) 2023-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#include "qwen3_omni.hpp"
+
+#include "module_genai/utils/profiler.hpp"
+#include "utils.hpp"
+
+namespace ov::genai::module {
+
+LLMInferencePAModule_Qwen3OMNI::InputsParams::PTR LLMInferencePAModule_Qwen3OMNI::parse_inputs(
+    LLMInferencePAModule_Qwen3OMNI::InputsParams::PTR inputs_params) {
+     auto cur_inputs = inputs_params ? inputs_params : std::make_shared<InputsParamsQwen3_OMNI>();
+
+    return std::static_pointer_cast<InputsParams>(cur_inputs);
+}
+
+LLMInferencePAModule_Qwen3OMNI::LLMInferencePAModule_Qwen3OMNI(const IBaseModuleDesc::PTR& desc,
+                                                           const PipelineDesc::PTR& pipeline_desc,
+                                                           const VLMModelType& model_type)
+    : LLMInferencePAModule(desc, pipeline_desc, model_type) {
+    //
+    
+}
+
+void LLMInferencePAModule_Qwen3OMNI::run() {
+    GENAI_INFO("Running module: " + module_desc->name);
+    std::string generated_text;
+
+    prepare_inputs();
+    auto inputs_params = std::dynamic_pointer_cast<InputsParamsQwen3_OMNI>(parse_inputs());
+
+    
+    // GENAI_INFO("LLM output: " + generated_text);
+    // this->outputs["generated_text"].data = generated_text;
+}
+
+namespace LLMInferenceSDPAModule_Utils {
+
+}  // namespace LLMInferenceSDPAModule_Utils
+
+}  // namespace ov::genai::module
---
+++ src/cpp/src/module_genai/modules/m_llm_inference_pa/models/qwen3_omni.hpp
@@ -0,0 +1,43 @@
+// Copyright (C) 2023-2026 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <filesystem>
+#include <openvino/runtime/tensor.hpp>
+#include <string>
+
+#include "module_genai/modules/m_llm_inference_pa/md_llm_inference_pa.hpp"
+
+namespace ov::genai::module {
+
+
+class LLMInferencePAModule_Qwen3OMNI : public LLMInferencePAModule {
+public:
+    LLMInferencePAModule_Qwen3OMNI(const IBaseModuleDesc::PTR& desc, const PipelineDesc::PTR& pipeline_desc, const VLMModelType& model_type);
+
+    void run() override;
+
+protected:
+    struct InputsParamsQwen3_OMNI : public InputsParams {
+    public:
+        DeclareClass_PTR_Create(InputsParamsQwen3_OMNI);
+        ov::Tensor attention_mask;
+
+        ov::Tensor visual_embeds;
+        ov::Tensor visual_pos_mask;
+        std::vector<ov::Tensor> grid_thw;
+        ov::Tensor position_ids;
+        ov::Tensor rope_deltas;
+        std::optional<std::vector<ov::Tensor>> deepstack_embeds = std::nullopt;
+    };
+    InputsParams::PTR parse_inputs(InputsParams::PTR inputs_params = nullptr) override;
+
+private:
+};
+
+namespace LLMInferencePAModule_Utils {
+
+}  // namespace LLMInferencePAModule_Utils
+
+}  // namespace ov::genai::module
